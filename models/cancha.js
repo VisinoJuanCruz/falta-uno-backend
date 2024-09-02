533600cc84927a351029a3415a31193892045287
visinodeveloper <visinodeveloper@gmail.com>
2024-09-02 02:49:15 -0300

    end feature

--- models/cancha.js
+++ models/cancha.js
@@ -1,6 +1,5 @@
-//Archivo: ./backend/models/cancha.js
+// Archivo: ./backend/models/cancha.js
 const mongoose = require('mongoose');
-
 
 const canchaSchema = new mongoose.Schema({
   capacidadJugadores: { type: String, required: true },
@@ -8,6 +7,8 @@ const canchaSchema = new mongoose.Schema({
   materialPiso: { type: String, required: true },
   complejoAlQuePertenece: { type: mongoose.Schema.Types.ObjectId, ref: 'Complejo' },
   precio: { type: Number, required: true },
+  horaDeSubida: { type: Number }, // Hora en formato 24 horas a partir de la cual sube el precio (e.g., 18 para las 18:00)
+  precioDeSubida: { type: Number }, // Nuevo precio a partir de la horaDeSubida
   reservas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reserva' }], // Array to store reservations
   imagen: { type: String, required: true },
   nombre: { type: String, required: true },

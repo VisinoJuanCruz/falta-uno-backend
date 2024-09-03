b1d7b309b1190cd66fb9d5408cdeafe7fe97af55
visinodeveloper <visinodeveloper@gmail.com>
2024-09-03 02:39:56 -0300

    end feature

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,34 @@ const { sendWelcomeEmail, sendVerificationEmail } = require('../utils/mailer');
 
 const router = express.Router();
 
+
 // Ruta para el inicio de sesión
+router.post('/login', async (req, res) => {
+  passport.authenticate('local', { session: false }, (err, user, info) => {
+    if (err) {
+      console.error('Error en autenticación local:', err);
+      return res.status(500).json({ message: 'Error en la autenticación local' });
+    }
+    if (!user) {
+      console.log('Usuario no encontrado');
+      return res.status(401).json({ message: 'Usuario no encontrado en la base de datos' });
+    }
+    req.logIn(user, { session: false }, async (err) => {
+      if (err) {
+        console.error('Error al iniciar sesión:', err);
+        return res.status(500).json({ message: 'Error al iniciar sesión' });
+      }
+      console.log('Inicio de sesión exitoso');
+      await user.populate('complejos');
+      // Generar token JWT
+      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+      // Devolver los datos del usuario sin la contraseña
+      const userData = { _id: user._id, name: user.name, mail: user.mail, whatsapp: user.whatsapp, equiposCreados: user.equiposCreados, role: user.role, complejos: user.complejos };
+      return res.json({ token, user: userData });
+    });
+  })(req, res);
+});
+
 
 router.post('/register', async (req, res) => {
   try {

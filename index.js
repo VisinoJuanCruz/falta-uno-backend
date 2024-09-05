37936354fcc33b3461524e9d3b42cf19b8b77ebe
visinodeveloper <visinodeveloper@gmail.com>
2024-09-05 13:48:07 -0300

    adjust origin

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors({
-  origin: 'https://www.somosfulbo.com/',
+  origin: 'https://www.somosfulbo.com',
   credentials: true,
 }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://www.somosfulbo.com/');
+  res.header('Access-Control-Allow-Origin', 'https://www.somosfulbo.com');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');

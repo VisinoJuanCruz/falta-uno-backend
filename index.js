e476e3a8ad3caf5b048d46980488da1d4b88abab
visinodeveloper <visinodeveloper@gmail.com>
2024-09-19 02:33:11 -0300

    fix

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors({
-  origin: 'https://www.somosfulbo.com',
+  origin: '*',
   credentials: true,
 }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://www.somosfulbo.com');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
@@ -39,7 +39,7 @@ app.use('/images', express.static('images'));
 
 
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/main`);
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/develop`);
 
 // Configuración de express-session
 app.use(session({

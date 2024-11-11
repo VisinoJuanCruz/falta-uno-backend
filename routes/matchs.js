a0ce1a7795d5965155f6dbdfb635f619e928ad06
visinodeveloper <visinodeveloper@gmail.com>
2024-11-11 15:50:21 -0300

    add controller for get players by ids

--- routes/matchs.js
+++ routes/matchs.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 
 // Agregar un partido
 router.post('/:teamId/add-match', async (req, res) => {
+  console.log(req.body)
   try {
     const { tipoPartido, equipo, equipoOponente, jugadoresEquipo, jugadoresOponente, fecha, resultado } = req.body;
 
@@ -25,7 +26,7 @@ router.post('/:teamId/add-match', async (req, res) => {
     let equipoOponenteExistente;
     if (tipoPartido === 'Entrenamiento') {
       req.body.equipoOponente = equipo;
-      req.body.jugadoresOponente = jugadoresEquipo.map(j => ({ jugador: j.jugador, goles: j.goles }));
+      req.body.jugadoresOponente = jugadoresOponente.map(j => ({ jugador: j.jugador, goles: j.goles }));
     } else if (tipoPartido === 'Amistoso') {
       if (mongoose.Types.ObjectId.isValid(equipoOponente)) {
         equipoOponenteExistente = await Team.findById(equipoOponente);
@@ -41,10 +42,11 @@ router.post('/:teamId/add-match', async (req, res) => {
         return res.status(400).json({ error: 'En torneos, el equipo oponente debe estar registrado en la plataforma' });
       }
     }
-
+    
     // Crear y guardar el partido
     const match = new Match(req.body);
     await match.save();
+    console.log("SE GUARDA ESTO:",match)
 
     // Agregar el partido al equipo y, si aplica, al equipo oponente
     equipoExistente.matches.push(match._id);
@@ -97,5 +99,40 @@ router.post('/:teamId/add-match', async (req, res) => {
     res.status(500).json({ error: 'Hubo un problema al crear el partido' });
   }
 });
+
+// Obtener partidos de un equipo
+router.get('/:teamId/matches', async (req, res) => {
+  try {
+    const { teamId } = req.params;
+
+    // Verificar si el equipo existe
+    const equipoExistente = await Team.findById(teamId);
+    if (!equipoExistente) {
+      return res.status(400).json({ error: 'El equipo no existe' });
+    }
+
+    // Obtener los partidos del equipo
+    const partidos = await Match.find({
+      $or: [{ equipo: teamId }, { equipoOponente: teamId }]
+    })
+      .populate('equipo', 'nombre escudo')
+      .populate('equipoOponente', 'nombre escudo')
+      .populate('jugadoresEquipo.jugador', 'name image') // Información de los jugadores del equipo
+      .populate('jugadoresOponente.jugador', 'name image'); // Información de los jugadores del equipo oponente
+
+    // Si no hay partidos, responder con un mensaje adecuado
+    if (partidos.length === 0) {
+      return res.status(404).json({ message: 'Este equipo no ha jugado ningún partido aún' });
+    }
+
+    res.status(200).json(partidos);
+  } catch (error) {
+    console.error('Error al obtener los partidos:', error);
+    res.status(500).json({ error: 'Hubo un problema al obtener los partidos' });
+  }
+});
+
+module.exports = router;
+
 
 module.exports = router;
